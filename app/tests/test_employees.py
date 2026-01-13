a694fee280081ba0125bde83e8d242fe2352c38d
chandraprakash4790 <chandraprakashmirpati@gmail.com>
2026-01-13 13:47:04 +0530

    update test_create_employee: change test data to use consistent employee details

--- app/tests/test_employees.py
+++ app/tests/test_employees.py
@@ -9,5 +9,5 @@ def get_token():
 def test_create_employee():
     token = get_token()
     res = client.post("/api/employees/", headers={"Authorization": f"Bearer {token}"},
-                      json={"name": "Alice1", "email": "alice@test.com"})
+                      json={"name": "Vijay", "email": "vijay@test.com"})
     assert res.status_code == 201
